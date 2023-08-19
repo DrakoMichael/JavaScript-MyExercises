9cb518e92b74fcf2a19bbeda8b2aaf1d027b8844
Michael mello <dakton2001@Gmail.com>
2023-08-18 22:14:37 -0300

    Atualização com mais Js

--- Aulas/aula04/Scripts/Main.js
+++ Aulas/aula04/Scripts/Main.js
@@ -1,3 +1,19 @@
 window.alert('Olá, Mundo');
 window.confirm('Tudo bem com você?');
 window.prompt('qual é seu nome?');
+
+//para que serve
+//Não é necessário ; para fechar igual o CSS
+
+window.alert('Olá, Mundo'); //Alerta de mensagem através do navegador
+window.confirm('Tudo bem com você?'); //Alerta de mensagem através do navegador com botão de confirmação
+window.prompt('qual é seu nome?'); //Alerta de mensagem através do navegador com caixa de input de texto e confirmação
+
+window.alert('Olá, Mundo'); 
+//window -> tela / Alerta -> alerta / (aqui dentro coloca o dado) - com aspas simples fica texto, sem é numero ou dado
+
+window.confirm('Tudo bem com você?'); 
+//window -> tela / Confirm -> caixa com confirmação
+
+window.prompt('qual é seu nome?');
+//Window -> tela / prompt -> caixa de entrada de texto - input de informação
