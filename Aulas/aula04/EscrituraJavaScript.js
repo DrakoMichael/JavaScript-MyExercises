3c0383411158af3881790ae093bfc03e9068dc67
Michael mello <dakton2001@Gmail.com>
2023-08-19 02:59:50 -0300

    Atualização

--- Aulas/aula04/EscrituraJavaScript.js
+++ Aulas/aula04/EscrituraJavaScript.js
@@ -10,13 +10,48 @@ window.confirm('Tudo bem com você?'); //Alerta de mensagem através do navegado
 window.prompt('qual é seu nome?'); //Alerta de mensagem através do navegador com caixa de input de texto e confirmação
 
 window.alert('Olá, Mundo'); 
-//window -> tela / Alerta -> alerta / (aqui dentro coloca o dado) - com aspas simples fica texto, sem é numero ou dado
+//window -> tela / Alerta -> alerta / (aqui dentro coloca o dado) - com aspas simples fica texto, sem aspas é numero ou dado (possui somente um botão ''OK'')
 
 window.confirm('Tudo bem com você?'); 
-//window -> tela / Confirm -> caixa com confirmação
+//window -> tela / Confirm -> caixa com confirmação, COM DUAS OPÇÕES (true/false)
 
 window.prompt('qual é seu nome?');
 //Window -> tela / prompt -> caixa de entrada de texto - input de informação
+
+//Desse jeito está sem recebimento de dados (guardar dados), é usado as variáveis. -> VAR - let <- para registrar variáveis
+//
+//
+
+
+//variáveis - Existem 3 formas de delimitar uma string -> "exemplo" - 'exemplo' - `exemplo` <-
+
+
+//identificadores/variáveis - Podem começar com -> Letra - $ - _ <-
+//É possível usar -> Letras - números <-
+//É possível usar -> Acentos - símbolos <-
+//Não pode conter -> espaços - palavras reservada(palavras nativas da linguagem) <-
+//Não podem começar com números
+
+//Boas práticas para escolher nome de identificadores/variáveis
+//Maiúsculas e minúsculas fazem difença | é possível criar variável com a mesma letra, porém a unica mudança sendo maiúscula ou minúscula <- atenção a isso
+//Tentar usar nomes coerentes para as variáveis (Exemplo: nome = "nomealgumacoisa" | idade = "idadealgumacoisa" | cor = "coralgumacoisa")
+//Evitar se tornar um 'programador alfabato' ou um 'programador contador'
+
+//Tipos primitivos resumido | Se resume a -> Number - Booleano - String
+//5 18 -1 <- exemplo de número inteiro | continua sendo um tipo Number(numérico)
+//0.5 -15.9 3.15 8.0 <- exemplo de número real | continua sendo um tipo Number(numérico)
+//True/false <- exemplo de booleano(boolean)
+//"Google" - 'javaScript' - `Maria` <- exemplo de String | quando está dentro de aspás, não é considerado um dado Number(numérico)
+
+//Todos tipos primitivos | 'typeof' no console para ver qual o tipo primitivo
+//Number -> Infinity - NaN(not a number)
+//String 
+//Boolean
+//null -> nulo, sem informação ou dado (não é igual a zero)
+//Undefinid -> sem definição
+//Object -> nulo pode ser considerado object | Array -> um objeto, um vetor
+//Function
+
 
 //-----------------------------------------------------------------
 //Teste Github - ignore! ------------------------------------------
